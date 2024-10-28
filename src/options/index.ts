ce24d6db5f225ae71fb320b97db50b67669a837b
Hexin <hexium310@gmail.com>
2024-10-29 08:07:59 +0900

    Stop using insertAdjacentHTML

--- src/options/index.ts
+++ src/options/index.ts
@@ -1,30 +1,6 @@
 import { SpotifyNowplayingStorage } from '~types';
 import { login } from '~utils';
 
-const { userName, isPremium } = await chrome.storage.local.get(['userName', 'isPremium']) as SpotifyNowplayingStorage;
-
-const mainElement = document.getElementById('main') as HTMLElement;
-if (userName && !isPremium) {
-  mainElement.insertAdjacentHTML(
-    'afterbegin',
-    `<p class="notPremium alert alert-danger">
-      ログイン中のユーザーは Spotify Premium ではありません。<br>
-      Spotify-NowPlaying は Spotify Premium でのみ動作します。
-    </p>`
-  );
-}
-
-if (userName) {
-  mainElement.insertAdjacentHTML(
-    'afterbegin',
-    `<p>
-      <i class="bi-person-fill-check">
-      </i>
-      ${userName}
-    </p>`
-  );
-}
-
 const loginElement = document.getElementById('login') as HTMLElement;
 loginElement.addEventListener('click', async () => {
   await login();
@@ -33,4 +9,17 @@ loginElement.addEventListener('click', async () => {
 
 const redirectUrl = chrome.identity.getRedirectURL();
 const redirectUrlElement = document.getElementById('redirectUrl') as HTMLSpanElement;
-redirectUrlElement.innerText = redirectUrl;
+redirectUrlElement.insertAdjacentText('afterbegin', redirectUrl);
+
+const { userName, isPremium } = await chrome.storage.local.get(['userName', 'isPremium']) as SpotifyNowplayingStorage;
+
+if (userName && !isPremium) {
+  const element = document.getElementById('notPremium') as HTMLParagraphElement;
+  element.style.display = 'initial';
+}
+
+if (userName) {
+  const element = document.getElementById('userName') as HTMLParagraphElement;
+  element.style.display = 'initial';
+  element.insertAdjacentText('beforeend', userName);
+}
